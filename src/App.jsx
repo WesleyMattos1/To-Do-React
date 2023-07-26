179296c6ae35d27e4c56730d2dba98b5fe1c2c73
WesleyMattos1 <wesleymattos025@gmail.com>
2023-07-26 13:58:25 -0300

    ultimate

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Todo from "./components/Todo";
 
 import TodoForm from "./components/TodoForm";
 
+import Search from "./components/search";
+
 import "./App.css";
 
 function App() {
@@ -11,45 +13,55 @@ function App() {
     {
       id: 1,
       text: "adicionar uma funcionalidade no sistema",
-      category: "trabalho",
+      category: "Exemplo",
       iscomplete: false,
     },
     {
       id: 2,
       text: "Ir para academia",
-      category: "Pessoal",
+      category: "Exemplo",
       iscomplete: false,
     },
     {
       id: 3,
       text: "Estudar react",
-      category: "Estudos",
-      iscomplete: false,
-    },
-    {
-      id: 4,
-      text: "adicionar uma funcionalidade no sistema",
-      category: "trabalho",
-      iscomplete: false,
-    },
-    {
-      id: 5,
-      text: "Ir para academia",
-      category: "Pessoal",
-      iscomplete: false,
-    },
-    {
-      id: 6,
-      text: "Estudar react",
-      category: "Estudos",
+      category: "Exemplo",
       iscomplete: false,
     },
   ]);
-
+  const [search, setSearch] = useState("");
+  /**
+   *  Função para remoção de uma TODO
+   * Recebe como parâmetro o id da TODO e retorna void (Nada)
+   * @example {id: 6}
+   */
   const removetodo = (id) => {
+    /**
+     * Variável que recebe uma 'cópia' do array de TODOS
+     * (Usando a técnica com spread operator) Que basicamente copia todas as informações contidas na lista
+     */
     const newTodos = [...todos];
+
+    /**
+     * Realiza o filtro dentro do array copiado de TODOS
+     * Onde são retornados todos os registros que não possuam o id igual ao recebido por parametro
+     * @example {newTodos.filter(todo => todo.id !== 6)}
+     */
     const filteredTodos = newTodos.filter((todo) => todo.id !== id);
+
+    /**
+     * Define o estado com a nova lista de TODOS
+     * Na qual não contém o registro com id recebido por parâmetro
+     * @example {id: 6} Não vai existir na lista
+     */
     setTodos(filteredTodos);
+  };
+  const completeTodo = (id) => {
+    const newTodos = [...todos];
+    newTodos.map((todo) =>
+      todo.id === id ? (todo.iscomplete = !todo.iscomplete) : todo
+    );
+    setTodos(newTodos);
   };
 
   const addTodo = (text, category) => {
@@ -68,11 +80,21 @@ function App() {
     <>
       <div className="app">
         <h1>Lista de tarefas</h1>
+        <Search Search={Search} setSearch={setSearch} />
         <div className="content">
           <div className="items">
-            {todos.map((todo) => (
-              <Todo key={todo.id} todo={todo} removeTodo={removetodo} />
-            ))}
+            {todos
+              .filter((todo) =>
+                todo.text.toLowerCase().includes(search.toLowerCase())
+              )
+              .map((todo) => (
+                <Todo
+                  key={todo.id}
+                  todo={todo}
+                  completeTodo={completeTodo}
+                  removeTodo={removetodo}
+                />
+              ))}
           </div>
           <div className="form">
             <TodoForm addTodo={addTodo} />
@@ -82,5 +104,4 @@ function App() {
     </>
   );
 }
-
 export default App;
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,14 +1,19 @@
 import React from "react";
 
-const Todo = ({ todo, removeTodo }) => {
+const Todo = ({ todo, removeTodo, completeTodo }) => {
   return (
-    <div className="card todo">
+    <div
+      className="card todo"
+      style={{ textDecoration: todo.iscomplete ? "line-through" : "" }}
+    >
       <div className="todo-content">
         <p>{todo.text}</p>
         <p className="category">({todo.category})</p>
       </div>
       <div>
-        <button className="btn-complete">Completar</button>
+        <button className="btn-complete" onClick={() => completeTodo(todo.id)}>
+          Completar
+        </button>
         <button className="btn-remove" onClick={() => removeTodo(todo.id)}>
           X
         </button>
